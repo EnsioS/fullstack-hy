1c6e5395ccc2385903e55ddc6ef66ce0183e8267
EnsioS <ensiosuonpera@luukku.com>
2018-01-17 10:10:02 +0200

    teht 1.3

--- osa1/half/src/index.js
+++ osa1/half/src/index.js
@@ -10,9 +10,9 @@ const Otsikko = (props) => {
 const Sisalto = (props) => {
   return (
     <div>
-      < Osa osa={props.osa1} tehtavia={props.tehtavia1}/>
-      < Osa osa={props.osa2} tehtavia={props.tehtavia2}/>
-      < Osa osa={props.osa3} tehtavia={props.tehtavia3}/>
+      < Osa osa={props.osa1.nimi} tehtavia={props.osa1.tehtavia}/>
+      < Osa osa={props.osa2.nimi} tehtavia={props.osa2.tehtavia}/>
+      < Osa osa={props.osa3.nimi} tehtavia={props.osa3.tehtavia}/>
     </div>
   )
 }
@@ -25,24 +25,30 @@ const Osa = (props) => {
 
 const Yhteensa = (props) => {
   return (
-    <p>yhteensä {props.tehtavia1 + props.tehtavia2 + props.tehtavia3} tehtävää</p>
+    <p>yhteensä {props.osa1.tehtavia + props.osa2.tehtavia + props.osa3.tehtavia} tehtävää</p>
   )
 }
 
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+  const osa1 = {
+    nimi: 'Reactin perusteet',
+    tehtavia: 10
+  }
+  const osa2 = {
+    nimi: 'Tiedonvälitys propseilla',
+    tehtavia: 7
+  }
+  const osa3 = {
+    nimi: 'Komponenttien tila',
+    tehtavia: 14
+  }
 
   return (
     <div>
       <Otsikko kurssi={kurssi} />
-      <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3} />
-      <Yhteensa tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3} />
+      <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} />
+      <Yhteensa osa1={osa1} osa2={osa2} osa3={osa3} />
     </div>
   )
 }

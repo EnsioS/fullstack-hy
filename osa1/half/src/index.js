80e0f936b7940077bda83ff3e2100445656ef95e
EnsioS <ensiosuonpera@luukku.com>
2018-01-19 23:58:43 +0200

    teht 2.5 (jatkaa osan 1 tehtävää ja siksi tässä kansiossa)

--- osa1/half/src/index.js
+++ osa1/half/src/index.js
@@ -1,35 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Otsikko = (props) => <h1>{props.kurssi.nimi}</h1>
-
-const Sisalto = (props) => {
-  return (
-    <div>
-      {props.osat.map(osa => <Osa key={osa.id} osa={osa}/>)}
-    </div>
-  )
-}
-
-const Osa = (props) => <p>{props.osa.nimi} {props.osa.tehtavia}</p>
-
-const Yhteensa = (props) => {
-  const tehtavia = props.osat.reduce( (sum, osa) => sum + osa.tehtavia, 0)
-
-  return (
-    <p>yhteensä {tehtavia} tehtävää</p>
-  )
-}
-
-const Kurssi = (props) => {
-  return (
-    <div>
-      <Otsikko kurssi={props.kurssi} />
-      <Sisalto osat={props.kurssi.osat} />
-      <Yhteensa osat={props.kurssi.osat} />
-    </div>
-  )
-}
+import Kurssi from './components/Kurssi'
 
 const App = () => {
   const kurssit = [

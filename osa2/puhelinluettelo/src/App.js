c9a1a1ed7d593545ccf55d16f5ff83cc2f3e16a5
EnsioS <ensiosuonpera@luukku.com>
2018-01-22 22:05:01 +0200

    teht 2.9, puhelinluettelo osa 4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -5,10 +5,14 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas', number: '040-123456' }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Martti Tienari', number: '040-123456' },
+        { name: 'Arto Järvinen', number: '040-123456' },
+        { name: 'Lea Kutvonen', number: '040-123456' }
       ],
       newName: '',
-      newNumber: ''
+      newNumber: '',
+      filter: ''
     }
   }
 
@@ -38,9 +42,20 @@ class App extends React.Component {
   }
 
   render() {
+    const personsToShow =
+      this.state.persons
+      .filter(person => person.name.toLowerCase().includes(this.state.filter.toLowerCase()))
+
     return (
       <div>
-        <h2>Puhelinluettelo</h2>
+        <h1>Puhelinluettelo</h1>
+        rajaa näytettäviä 
+        <input
+          id="filter"
+          value={this.state.filter}
+          onChange={this.handleValueChange}        
+        />
+        <h2>Lisää uusi</h2>
         <form onSubmit={this.addPerson}>
           <div>
             nimi: 
@@ -62,7 +77,7 @@ class App extends React.Component {
             <button type="submit">lisää</button>
           </div>
         </form>
-        < Numerot persons={this.state.persons} />
+        < Numerot persons={personsToShow} />
       </div>
     )
   }

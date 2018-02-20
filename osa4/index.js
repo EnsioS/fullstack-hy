f174f484e662e4f9a785d98dbc42c6ce573a5b98
EnsioS <ensiosuonpera@luukku.com>
2018-02-20 12:05:30 +0200

    testävät 4.1 ja 4.2, blogilista osa 1 ja 2

---
+++ osa4/index.js
@@ -0,0 +1,38 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+if (process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
+
+const mongoUrl = process.env.MONGODB_URI
+
+mongoose
+  .connect(mongoUrl)
+  .then( () => {
+    console.log('connected to database', mongoUrl)
+  })
+  .catch( error => {
+    console.log(error)
+  })
+
+mongoose.Promise = global.Promise
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(middleware.logger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.error)
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
